b4b1d845d7d5d523fbf0e4d2c0c8d5e556f2b9ca
Alexis Destrez <krostar@users.noreply.github.com>
2025-08-24 22:24:28 +0200

    feat(devshells/base): add PROJECT_ROOT env variable

--- dogfood/harmony/devShells/base.nix
+++ dogfood/harmony/devShells/base.nix
@@ -5,11 +5,14 @@
 }:
 pkgs.mkShellNoCC
 {
-  shellHook = unit.lib.nixago.appendToShellHooks pkgs "";
+  shellHook =
+    (unit.lib.nixago.appendToShellHooks pkgs "")
+    + ''
+      export PROJECT_ROOT="$(git rev-parse --show-toplevel)"
+    '';
+
   nativeBuildInputs = with pkgs; [
     jq
     just
-    nix-diff
-    nix-tree
   ];
 }
